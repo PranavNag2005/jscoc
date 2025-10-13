ef8778d585a4640b37f63f686b840934b603762a
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-10-13 16:31:04 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+// alert("helelo") this will not work because we are using nodejs but in browser it will work
+
+//number
+let age=18
+//string
+let name="pranav"
+//boolean
+let isloggedin=true
+//null =>standalone value
+
+//we can place the value as null for the varibale like this
+
+let nullvariable=null;
+console.log(nullvariable); // this outputs null
+
+let undefineds;
+console.log(undefineds) // this outputs undefined because it was initialized but declared value
+
+//symobol =>unique
+
+//non primitive datatype
+// Object
+console.log(typeof name) //string
+console.log(typeof null) //object
+console.log(typeof undefined); // output was undefined
