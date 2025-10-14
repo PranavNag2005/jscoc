3518ae28c97026f2279a0d9bc47ff5ce4a667188
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-10-14 07:40:33 +0000

    basics conversions

--- basics/conversionoperations.js
+++ basics/conversionoperations.js
@@ -1,3 +1,26 @@
-let score=33
-console.log(typeof score);
-console.log(typeof score);
+// let score="pranav"
+// console.log(typeof score);
+
+// let valueInNumber=Number(score)
+// console.log(valueInNumber);
+
+// console.log(typeof score);
+
+//"33"->nummber
+//"33abc"->Nan type if string
+//undefined -> Nan type is undefined
+//null ->0 but typeof is object
+//"pranav"->nan but type is string
+// true ->1 false ->0
+
+let isloggedin="56"
+//1 ->true 0->false
+// ""=>false "pranav"=>true
+// let Booleanisloggedin=Boolean(isloggedin)
+// console.log(Booleanisloggedin);
+// console.log(typeof Booleanisloggedin)
+
+let n="23";
+let numbers=Number(n)
+console.log(numbers)
+console.log(typeof numbers);
