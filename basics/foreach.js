d4cfd820c9e795597a1ef118aa130490ca2120a6
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-10-22 06:42:20 +0000

    foreach loop

---
+++ basics/foreach.js
@@ -0,0 +1,32 @@
+// const coding=["java","js","python","c","html"]
+// // coding.forEach(function (item){
+// // console.log(item);
+
+// // })
+
+// // coding.forEach((item)=>{
+// //     console.log(`i like ${item} language`);
+    
+// // })
+// function printme(item){
+// console.log(item);
+
+// }
+// coding.forEach(printme)
+// coding.forEach((item,index,arr)=>
+//     console.log(item,index,arr)
+// );
+
+const mycoding=[
+    {name:"pranav",
+    age:"22"
+    },
+    {
+        name:"vamsi",
+        age:"23"
+    }
+]
+mycoding.forEach( (item)=>{
+    console.log(`my name is ${item.name} and my age was ${item.age}`);
+    
+})
