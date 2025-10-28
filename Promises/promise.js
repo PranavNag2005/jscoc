9fad2f7fe611053b02880ee7775333c671ffd459
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-10-28 07:41:10 +0000

    promises

---
+++ Promises/promise.js
@@ -0,0 +1,66 @@
+// // A promise is an object thaat represent Eventual completion or rejection of Asynchronous operation
+
+// const promise1=new Promise(function(resolve,reject){
+//     //do an async task
+//     //dbcalls,apicalls 
+//     setTimeout(function(){
+//         console.log("inside promise");
+//         resolve()
+//     },1000)
+// })
+// promise1.then(function(){
+//     console.log("inside then ");
+    
+// })
+
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//     console.log("async task completed");
+//     resolve()
+//     },1000)
+// }).then(function(){
+//     console.log("it was resolved successfully");
+    
+// })
+// const promiseThreee=new Promise(function(Resolve,Reject){
+//     setTimeout(function(){
+//         console.log("inside promise three")
+//         Resolve({username:"pranav",email:"pranav@gmail.com"});
+        
+//     },1000)
+// }).then(function(user){
+//     console.log(user);
+    
+// })
+
+const promiseFour=new Promise((resolve,reject)=>{
+    setTimeout(function(){
+        console.log("in promisefour");
+        let error=true;
+        if(!error){
+        resolve({username:"vamsi",password:"v@123"})
+        }
+        else{
+            reject("something went wrong! Tryagain later")
+        }
+
+
+        
+    },1000)
+})
+
+promiseFour.then(function(user){
+    console.log(user);
+    return user.username
+    
+})
+.then(function(username){
+    console.log(username);
+    
+})
+.catch(function(error){
+    console.log(error);
+    
+})
+.finally(()=>{console.log("promise is either resolved or rejected");
+})
